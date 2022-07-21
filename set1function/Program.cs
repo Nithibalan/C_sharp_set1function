e519511288d577660d82fe921c98e68d0d049b00
Nithitha <nithisathiyabalan@gmail.com>
2022-07-21 11:56:30 +0530

    Third commit

---
+++ set1function/Program.cs
@@ -0,0 +1,475 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace set1function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Enter the first number");
+            //int first = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the second  number");
+            //int second = Convert.ToInt32(Console.ReadLine());
+            //long result = sum(first, second);
+            //Console.WriteLine("Addition:"+result);
+            //long value = diff(first, second);
+            //Console.WriteLine("Subraction:" + value);
+            //long Ans = product(first, second);
+            //Console.WriteLine("Multiplication:" + Ans); Q1
+
+            
+            //Console.WriteLine("Enter the base number:");
+            //double basen = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the exponent number:");
+            //double exponent= double.Parse(Console.ReadLine());
+            //double result = power(basen, exponent);
+            //Console.WriteLine("The positive integer is:"+result);//Q2
+
+            //Console.WriteLine("Enter the length of water tank:");
+            //float len = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the width of water tank:");
+            //float wid = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the height of water tank:");
+            //float heig = float.Parse(Console.ReadLine());
+            //float result = watertank(len, wid, heig);
+            //Console.WriteLine("The volume of rectangular water tank:"+result);//Q3
+
+            //Console.WriteLine("Enter the radius of circle:");
+            //float radius = float.Parse(Console.ReadLine());
+            //double pi = 3.14;
+            //double result = circle(radius, pi, 2);
+            //Console.WriteLine("The perimeter of a circle is:"+result);//Q4
+
+            //Console.WriteLine("Enter the numerator value:" );
+            //double topvalue = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the denominator value:");
+            //double bottomvalue = double.Parse(Console.ReadLine());
+            //double quotient = deno(topvalue, bottomvalue);
+            //Console.WriteLine("The quotient is:"+quotient );
+            //double reminder = num(topvalue, bottomvalue);
+            //Console.WriteLine("The reminder is:"+reminder); //Q5
+
+            //Console.WriteLine("Enter the first number:");
+            //double first = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the second number:");
+            //double second = double.Parse(Console.ReadLine());
+            //double result = power(first, second);
+            //Console.WriteLine("The square of given number is:"+result);//Q6
+
+            //Console.WriteLine("Enter the value:");
+            //double num = double.Parse(Console.ReadLine());
+            //double result = root(num);
+            //Console.WriteLine("The square root of the value is:"+result); //Q7
+
+            //Console.WriteLine("Enter the breadth:");
+            //double breadth = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the height:");
+            //double height = double.Parse(Console.ReadLine());
+            //double result = triangle(1.0/2.0, breadth, height);
+            //Console.WriteLine("The area of a triangle is:"+result); //Q8
+
+            //Console.WriteLine("Enter the kilometers:");
+            //double kilo = double.Parse(Console.ReadLine());
+            //double result = miles(kilo, 1.609);
+            //Console.WriteLine("Kilometers to miles:" +result);  //Q9 
+
+            //Console.WriteLine("Enter the celsius:");
+            //float cel = float.Parse(Console.ReadLine());
+            //float result = fah(cel, 9 / 5, 32);
+            //Console.WriteLine("Celsius to Fahrenheit:"+result); //Q10
+
+            //Console.WriteLine("Enter the first number:");
+            //int first = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the second number:");
+            //int second = int.Parse(Console.ReadLine());
+            //swap( ref first  , ref second);
+            //Console.WriteLine("After swapping first is" + first + "and second is:" + second); //Q11
+
+            //Console.WriteLine("Enter the first number:");
+            //int first = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the second number:");
+            //int second = int.Parse(Console.ReadLine());
+            //withouttemp(ref  first, ref second); //Q12
+
+            //Console.WriteLine("Enter the first number:");
+            //int first = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the second number:");
+            //int second = int.Parse(Console.ReadLine());
+            //overflow(ref  first, ref second); //Q13
+
+            //Console.WriteLine("Enter the first number:");
+            //int first = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the second number:");
+            //int second = int.Parse(Console.ReadLine());
+            //int result = randomnumber(first, second);
+            //Console.WriteLine("The randomnumber is:"+result); //Q14
+
+            //Console.WriteLine("Enter the first number:");
+            //int first = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the second number:");
+            //int second = int.Parse(Console.ReadLine());
+            //acres(first, second, 43560);//Q15
+
+            //Console.WriteLine("Enter the first number:");
+            //float first = float.Parse(Console.ReadLine());
+            //double result = naturalnumbers(first, 1, 2);
+            //Console.WriteLine("The sum of natural numbers:" + result);//Q16
+
+            //Console.WriteLine("Enter the first number:");
+            //int first = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the second number:");
+            //int second = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the third number:");
+            //int third = int.Parse(Console.ReadLine());
+            //int result =threenumber(first, second, third);
+            //Console.WriteLine("The average of three number is:"+result);//Q17
+
+            //Console.WriteLine("Enter the first number:");
+            //float first = float.Parse(Console.ReadLine());
+            //double result = waiter(first, 5, 100);
+            //Console.WriteLine("The tip provided to waiter is:"+result); //Q18
+
+            //Console.WriteLine("Enter the Amount:");
+            //int amount = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the tax percentage:");
+            //int tax_percentage = int.Parse(Console.ReadLine());
+            //double result = tax(amount, tax_percentage);
+            //Console.WriteLine("The Tax percentage is:"+result); //Q19
+
+
+
+            //Console.WriteLine("Enter the basic pay:");
+            //double basicpay = double.Parse(Console.ReadLine());
+            //double result = salary(basicpay);
+            //Console.WriteLine("The grosspay is:"+result); //Q20
+
+            //Console.WriteLine("Enter the principal:");
+            //int P = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the TimePeriod:");
+            //int N = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Rate of Interest:");
+            //int R = int.Parse(Console.ReadLine());
+            //long result = interest(P, N, R);
+            //Console.WriteLine("The Simple Interest :"+result); //21
+
+            //Console.WriteLine("Enter the principal balance:");
+            //double p = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the number of times interest:");
+            //double n = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the number of times period :");
+            //double t = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the rate of interest :");
+            //double r = double.Parse(Console.ReadLine());
+            //compo( p, r, n, t); //Q22
+
+            //Console.WriteLine("Enter the height of the person in cm:");
+            //float Centi = float.Parse(Console.ReadLine());
+            //double ft = centimeter(Centi);
+            //Console.WriteLine("The height of the person in feet:" + ft);
+            //double inch = height(Centi);
+            //Console.WriteLine("The height of the person in inch:" + inch); //Q23
+
+            //Console.WriteLine("Enter the height:");
+            //double hg = double.Parse(Console.ReadLine());
+            //double height = BMI(hg);
+            //Console.WriteLine("Height in meters:" + height);
+            //Console.WriteLine("Enter the weight:");
+            //double kilo = double.Parse(Console.ReadLine());
+            //double weight = BMI1(kilo);
+            //Console.WriteLine("Weight in kilograms:" + weight);
+            //double BMI2 = (weight / height) * height;
+            //Console.WriteLine("BMI2:"+BMI2); //Q24
+
+            //Console.WriteLine("Enter the student name:");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Enter the student Roll no:");
+            //int rollno = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the student Tam:");
+            //int Tam = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the student Eng:");
+            //int Eng = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the student science:");
+            //int sci = int.Parse(Console.ReadLine());
+            //int result = marks(Tam, Eng, sci);
+            //Console.WriteLine("Result:"+result); //Q25
+           
+
+
+
+           
+        }
+    
+
+        #region MyRegion Q1
+        public static int sum(int i,int j)
+
+        {
+            return i+j;
+        }
+        #endregion
+
+        #region MyRegion
+        public static int diff(int i, int j)
+        {
+            return Math.Abs(i - j);
+        }
+        #endregion
+
+        #region MyRegion
+        public static int product(int i, int j)
+        {
+            return i * j;
+        }
+
+        #endregion
+
+        #region MyRegion Q2
+        public static double power(double i, double j)
+        {
+            return Math.Pow(i, j);
+        }
+        #endregion
+
+        #region MyRegion Q3
+        public static float watertank(float i, float j, float k)
+        {
+            return i * j * k;
+        }
+        #endregion
+       
+        #region MyRegion Q4
+        public static double circle(double i, double j,double k)
+        {
+            return i*j*k;
+        }
+        #endregion
+
+        #region MyRegion Q5
+        public static double deno(double i, double j)
+        {
+        return i/j;
+        }
+
+        public static double num(double i, double j)
+        {
+            return i % j;
+        }
+
+        #endregion
+
+        #region MyRegion Q6
+        public static double square(double i, double j)
+        {
+            return Math.Pow(i, j);
+        }
+        #endregion
+
+        #region MyRegion Q7
+        public static double root(double i)
+        {
+            return Math.Sqrt(i);
+        }
+        #endregion
+
+        #region MyRegion Q8
+        public static double triangle(double i, double j, double k)
+        {
+            return (i * j * k);
+        }
+        #endregion
+
+        #region MyRegion Q9
+
+        public static double miles(double i, double j)
+        {
+            return i / j;
+        }
+        #endregion
+
+        #region MyRegion Q10
+        public static float fah(float i, float j, float k)
+        {
+            return i * j + k;
+        }
+        #endregion
+
+        #region MyRegion Q11
+
+        public static void swap( ref int i, ref int j)
+        {
+            int temp = i;
+            i = j;
+            j= temp;
+
+        }
+
+        #endregion
+
+        #region MyRegion Q12
+        public static void withouttemp(ref int i, ref int j)
+        {
+            i = i + j;
+            j = i - j;
+            i = i - j;
+
+            Console.WriteLine("After swap first number is:"+i +"second number is" +j);
+        }
+        #endregion
+
+        #region MyRegion Q13
+        public static void overflow(ref int i, ref int j)
+        {
+            i = i ^ j;
+            j = i ^ j;
+            i = i ^ j;
+
+            Console.WriteLine("After swap first number is:" + i + " second number is" + j);
+        }
+
+        #endregion
+
+        #region MyRegion Q14
+        public static int randomnumber(int i, int j )
+        {
+            Random r = new Random();
+            int num = r.Next(i, j);
+            return num;
+        }
+
+
+        #endregion
+
+        #region MyRegion Q15
+        public static void acres(double i, double j, double k)
+        {
+            double sq = i * j;
+            double Ft = sq / k;
+
+            Console.WriteLine("The area of a field in acres:"+Ft);
+        }
+        #endregion
+
+        #region MyRegion Q16
+        public static float naturalnumbers(float i, float j, float k)
+        {
+           return (i*(i+j)/k);
+        }
+        #endregion
+
+        #region MyRegion Q17
+        public static int threenumber(int i, int j , int k)
+        {
+            int total = i+j+k;
+            int average = total/3;
+            return average;
+        }
+
+
+        #endregion
+
+        #region MyRegion Q18
+        public static float waiter(float i, float j, float k)
+        {
+            return (i * j / k);
+        }
+
+        
+        #endregion
+
+        #region MyRegion Q20
+        public static double salary(double i)
+        {
+            double DA = i * 0.25;
+            Console.WriteLine("Dearness Allowance:" + DA);
+            double TA = i * 0.05;
+            Console.WriteLine("Travel Allowing:" + TA);
+            double PF = (i + DA) * 0.08;
+            Console.WriteLine("Enter the Provident Fund:" + PF);
+            double grosspay = (i + DA + TA - PF);
+            return grosspay;
+        }
+
+                
+ 
+
+        #endregion
+
+        #region MyRegion Q21
+        public static int interest(int i, int j, int k)
+        {
+            return i * j * k / 100;
+        }
+        
+        #endregion
+
+        #region MyRegion Q22 
+        public static void compo(double p, double r, double n, double t)
+        {
+            double nt = (n * t);
+            double ci = (1 + r / n);
+            double c = Math.Pow(ci, nt);
+            double compound= (p * c);
+
+            Console.WriteLine("The compound interest is:"+compound);
+        }
+
+        #endregion
+
+        #region MyRegion Q23
+
+        public static double centimeter(double i)
+        {
+            double ft = i / 30.48;
+            return ft;
+        }
+
+        public static double height(double j)
+        {
+            double  inch =  j/ 2.54;
+            return inch;
+        }
+
+        #endregion
+
+        #region Region Q24
+        public static double BMI(double i)
+        {
+            double hg = i / 39.37;
+            return hg;
+        }
+
+        public static double BMI1(double i)
+        {
+            double kilo = (i * 1000);
+            return kilo;
+        }
+        #endregion
+
+        #region MyRegion Q25
+        public static int marks(int i, int j, int k)
+        {
+            int tot = i + j + k;
+            int average = tot / 3;
+            return average;
+        }
+
+
+        
+        #endregion
+
+        #region MyRegion Q19
+        public static double tax(double i,double j)
+         {
+            return (i/(1+j/100));
+         }
+
+        #endregion
+    }
+
+
+}
